664ed2bdf77c92892ed0e672b816a778e07244b0
Jasmine Tran <100793918+jasetran@users.noreply.github.com>
2025-07-01 16:44:11 -0700

    updating adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-amplify';
+import adapter from 'amplify-adapter';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
