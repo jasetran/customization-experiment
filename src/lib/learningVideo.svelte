907c6a465fa3098d1f5b9598c5357a2a5da29a3e
Jasmine Tran <100793918+jasetran@users.noreply.github.com>
2025-07-02 22:50:18 -0700

    unmuting the video

--- src/lib/learningVideo.svelte
+++ src/lib/learningVideo.svelte
@@ -30,7 +30,6 @@
         bind:this={videoElement}
         src="/assets/NASA-PBSKIDS-ice-video.mp4"
         autoplay
-        muted
         playsinline
         on:canplay={handleCanPlay}
         on:play={handlePlay}
