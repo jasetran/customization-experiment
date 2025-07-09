b522bc965c13f5af79040c0b0db3692e7834e4ab
Jasmine Tran <100793918+jasetran@users.noreply.github.com>
2025-07-08 23:15:15 -0700

    adding some more hairs

---
+++ src/hairs/hair-47-front.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { userState } from "../state.svelte.js";
+</script>
+
+<svg
+    width="100%"
+    viewBox="0 0 923 1056"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M685.5 480C682.044 469.5 691.5 223 426.456 223C186 223 218 457 210.5 472C174.5 307.5 287.456 178 426.456 178C675.956 178 723 412 685.5 480Z"
+        fill={userState.hairColor}
+    />
+</svg>
---
+++ src/hairs/hair-49-front.svelte
@@ -0,0 +1,119 @@
+<script>
+    import { userState } from "../state.svelte.js";
+</script>
+
+<svg
+    width="100%"
+    viewBox="0 0 923 1056"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M274.997 231.611C345.301 188.707 395.546 181.346 421.733 186.38L422.962 186.629L423.059 186.653C426.898 187.673 434.324 188.793 441.962 190.044C449.148 191.221 456.594 192.524 460.76 193.964C461.008 193.934 461.348 193.894 461.771 193.845C462.728 193.734 464.111 193.581 465.827 193.411C469.258 193.072 474.025 192.665 479.367 192.393C489.74 191.865 502.515 191.836 511.879 193.912C519.36 190.765 528.839 191.096 538.978 193.517C549.455 196.019 560.932 200.822 572.219 206.802C594.777 218.754 616.912 235.584 629.236 248.777L629.243 248.784C638.751 259.061 652.015 278.868 665.416 303.161C666.282 304.731 667.149 306.32 668.016 307.927C632.621 275.951 567.155 256.592 441.439 256.592C318.28 256.592 249.625 290.898 211.999 339.596C218.958 321.114 226.302 299.507 235.882 280.028C245.684 260.096 257.985 242.045 274.994 231.614L274.997 231.611Z"
+        fill={userState.hairColor}
+    />
+    <path
+        d="M651.007 526.581C594.521 545.089 667.32 483.869 702.494 436.396L686.565 405.092C635.014 478.892 594.116 565.08 651.007 526.581Z"
+        fill="url(#paint0_linear_2145_243)"
+    />
+    <path
+        d="M667.694 434.767C624.947 443.629 628.879 383.185 644.044 374.302L617.726 346C601.722 410.621 626.098 466.932 667.694 434.767Z"
+        fill="url(#paint1_linear_2145_243)"
+    />
+    <path
+        d="M591.632 270.117C544.089 315.899 472.791 276.23 502.921 242.125L422.421 242.125C416.383 324.991 563.038 329.186 591.632 270.117Z"
+        fill="url(#paint2_linear_2145_243)"
+    />
+    <path
+        d="M625.566 331.26C593.612 360.799 574.701 323.051 593.676 304.01L580.41 304.725C559.282 365.486 598.904 374.514 625.566 331.26Z"
+        fill="url(#paint3_linear_2145_243)"
+    />
+    <path
+        d="M226.5 334.5C264.529 355.652 290.374 308.378 267.437 294.36L280.496 291.921C315.384 345.968 262.627 370.229 226.5 334.5Z"
+        fill="url(#paint4_linear_2145_243)"
+    />
+    <path
+        d="M665.501 511C665.501 551.339 676.593 254.724 441.286 254.724C196.642 254.724 211 503.339 211 463C164 349.5 212.399 172.113 436.593 166C730 158 767.001 461 665.501 511Z"
+        fill={userState.hairColor}
+    />
+    <path
+        d="M273.5 272C310.296 318.256 384.267 281.728 367.767 248.25L424.767 248.25C430.804 331.116 302.094 331.069 273.5 272Z"
+        fill="url(#paint5_linear_2145_243)"
+    />
+    <defs>
+        <linearGradient
+            id="paint0_linear_2145_243"
+            x1="704.393"
+            y1="406.989"
+            x2="649.216"
+            y2="549.312"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color={userState.hairColor} />
+            <stop
+                offset="1"
+                stop-color={userState.hairColor}
+                stop-opacity="0"
+            />
+        </linearGradient>
+        <linearGradient
+            id="paint1_linear_2145_243"
+            x1="679.14"
+            y1="386.125"
+            x2="603.503"
+            y2="438.539"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color={userState.hairColor} />
+            <stop
+                offset="1"
+                stop-color={userState.hairColor}
+                stop-opacity="0"
+            />
+        </linearGradient>
+        <linearGradient
+            id="paint2_linear_2145_243"
+            x1="510.951"
+            y1="327.484"
+            x2="523.355"
+            y2="246.598"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color={userState.hairColor} stop-opacity="0" />
+            <stop offset="1" stop-color={userState.hairColor} />
+        </linearGradient>
+        <linearGradient
+            id="paint3_linear_2145_243"
+            x1="574.375"
+            y1="376.709"
+            x2="590.033"
+            y2="294.521"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color={userState.hairColor} stop-opacity="0" />
+            <stop offset="1" stop-color={userState.hairColor} />
+        </linearGradient>
+        <linearGradient
+            id="paint4_linear_2145_243"
+            x1="303.37"
+            y1="360.44"
+            x2="268.734"
+            y2="284.279"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color={userState.hairColor} stop-opacity="0" />
+            <stop offset="1" stop-color={userState.hairColor} />
+        </linearGradient>
+        <linearGradient
+            id="paint5_linear_2145_243"
+            x1="359.737"
+            y1="333.609"
+            x2="347.333"
+            y2="252.723"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color={userState.hairColor} stop-opacity="0" />
+            <stop offset="1" stop-color={userState.hairColor} />
+        </linearGradient>
+    </defs>
+</svg>
