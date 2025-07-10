49e9e8c167a1114747dcc123a3c9aba27c1429e7
Jasmine Tran <100793918+jasetran@users.noreply.github.com>
2025-07-10 16:20:35 -0700

    adding some hairs and clothes

---
+++ src/hairs/hair-54-back.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { userState } from "../state.svelte.js";
+</script>
+
+<svg
+    width="100%"
+    viewBox="0 0 923 1056"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <ellipse
+        cx="429.5"
+        cy="472.5"
+        rx="193.5"
+        ry="191.5"
+        fill={userState.hairColor}
+    />
+</svg>
